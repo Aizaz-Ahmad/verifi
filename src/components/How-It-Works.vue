49127abea931bcf2e094ec5fd119d5afa32bbba5
Aizaz <aizazahamd90@gmail.com>
2020-12-04 21:32:45 +0500

    check

--- src/components/How-It-Works.vue
+++ src/components/How-It-Works.vue
@@ -5,19 +5,22 @@
             <p class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum</p>
             <div class="features">
                 <div class="feature">
-                    <div class="img"><img src="./../assets/images/Web/How-It-works-1.png" alt=""></div>
+                    <div class="img desktop-img"><img src="./../assets/images/Web/How-It-works-1.png" alt=""></div>
+                    <div class="img mobile-img"><img src="./../assets/images/Web/HIW-1-Mobile.png" alt=""></div>
                     <div class="work-description">
                         <p>Pick a contract, which you want to analyze</p>
                     </div>
                 </div>
                 <div class="feature">
-                    <div class="img"><img src="./../assets/images/Web/How-It-works-2.png" alt=""></div>
+                    <div class="img desktop-img"><img src="./../assets/images/Web/How-It-works-2.png" alt=""></div>
+                    <div class="img mobile-img"><img src="./../assets/images/Web/HIW-2-Mobile.png" alt=""></div>
                     <div class="work-description">
                         <p>Our sophisticated scanners will look for a bug</p>
                     </div>
                 </div>
                 <div class="feature">
-                    <div class="img"><img src="./../assets/images/Web/How-It-works-3.png" alt=""></div>
+                    <div class="img desktop-img"><img src="./../assets/images/Web/How-It-works-3.png" alt=""></div>
+                    <div class="img mobile-img"><img src="./../assets/images/Web/HIW-3-Mobile.png" alt=""></div>
                     <div class="work-description">
                         <p>secure your earnings and make more money</p>
                     </div>
@@ -37,7 +40,6 @@ export default {
 <style scoped>
     .container{
         background-color:var(--main-text-color);
-        /* padding-top:calc(190 / 1080 * 100vh); */
     }
     .header{
         font-size: calc(50 / 1920 * 100vw);
@@ -91,5 +93,51 @@ export default {
     }
     .feature .img{
         text-align: center;
+    }
+    .desktop-img{
+        display:block;
+    }
+    .mobile-img{
+        display:none;
+    }
+    @media screen  and (max-width:768px){
+        .desktop-img{
+            display:none;
+        }
+        .mobile-img{
+            display:block;
+        }
+        .content{
+            width: calc(335 / var(--standard-mobile-width) * 100vw);
+        }
+        .header{
+            font-size: calc(30 / var(--standard-mobile-width) * 100vw);
+        }
+        p.description{
+            font-size: calc(14 / var(--standard-mobile-width) * 100vw);
+            margin: calc(30 / var(--standard-mobile-height) * 100vh) 0px;
+        }
+        .features{
+            flex-direction: column;
+            margin-top: calc(40 / var(--standard-mobile-height) * 100vh);
+        }
+        .features .feature{
+            width: 100%;
+            height: calc(75 / var(--standard-mobile-height) * 100vh);
+            margin-bottom: calc(20 / var(--standard-mobile-height) * 100vh);
+            flex-direction: row;
+            align-items: center;
+        }
+        .feature .work-description{
+            margin-top:0px;
+            width: auto;
+        }
+        .feature .work-description p{
+            font-size: calc(16 / var(--standard-mobile-width) * 100vw);
+            line-height: 1;
+        }
+        .feature .img{
+            margin: 0px 5px;
+        }
     }
 </style>
